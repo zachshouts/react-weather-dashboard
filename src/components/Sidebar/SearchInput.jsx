f8971514e5f68da7f8a775c3d1a000614a852d1d
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-03-04 17:12:23 -0600

    Added geolocation

--- src/components/Sidebar/SearchInput.jsx
+++ src/components/Sidebar/SearchInput.jsx
@@ -1,8 +1,8 @@
 import { Input, Button } from '@chakra-ui/react';
 import { BiSearchAlt, BiCurrentLocation } from 'react-icons/bi';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-const SearchInput = ({weatherData, setWeatherData, ready, setReady}) => {
+const SearchInput = ({ setWeatherData, setReady }) => {
 
     const [ search, setSearch ] = useState('');
 
@@ -23,11 +23,10 @@ const SearchInput = ({weatherData, setWeatherData, ready, setReady}) => {
         const currentWeatherUrl = `https://api.weatherapi.com/v1/current.json?q=${lat},${lon}&key=${process.env.REACT_APP_WEATHER_API}`;
         const currentWeather = await fetch(currentWeatherUrl);
         const currentWeatherData = await currentWeather.json();
-        // console.log(currentWeatherData);
+
         const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?q=${lat},${lon}&days=7&key=${process.env.REACT_APP_WEATHER_API}`;
         const forecast = await fetch(forecastUrl);
         const forecastData = await forecast.json();
-        // console.log(forecastData);
 
         setWeatherData([ currentWeatherData, forecastData.forecast]);
         setReady(true);
@@ -36,16 +35,26 @@ const SearchInput = ({weatherData, setWeatherData, ready, setReady}) => {
     const handleSearch = () => {
         fetchCityCoords();
     }
-
-    // useEffect(() => {
-    //     fetchCityCoords()
-    // }, []);
+    
+    
+    const handleGeolocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(handlePosition);
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
+    }
+    
+    const handlePosition = (position) => {
+        fetchWeatherData(position.coords.longitude, position.coords.latitude);
+    }
+    
 
     return (
         <div className='row flex justify-center items-center mt-8 mx-12'>
             <Button type='button' variant='ghost' borderRadius='full' onClick={handleSearch}><BiSearchAlt /></Button>
             <Input placeholder='Search for places...' value={search} name='search' variant='unstyled' width='xs' onChange={(e) => { setSearch(e.target.value) }}/>
-            <Button size='sm' type='button' borderRadius='full' disabled><BiCurrentLocation /></Button> 
+            <Button size='sm' type='button' borderRadius='full' onClick={() => handleGeolocation()}><BiCurrentLocation /></Button> 
         </div>
     );
 }
