712fef262f962ce64559ff2343fb46c789f50185
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-03-04 16:02:08 -0600

    Removed unused card and file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,8 @@
 import MainNav from './MainNav';
 import SevenDay from './SevenDay';
 import HourlyForecast from './HourlyForecast';
-import HighlightCardOne from './HighlightCardOne';
 import { BasicCard, UVCard, SunCard } from './cards';
 import { useState } from 'react';
-import { BsArrowUp } from 'react-icons/bs';
 
 
 const Main = ({weatherData}) => {
@@ -36,23 +34,18 @@ const Main = ({weatherData}) => {
                             <UVCard title='UV Index' value={weatherData[0].current.uv} />
                         </div>
                         <div className=''>
-                            {/* <HighlightCardOne data={{title: 'Wind Status', value: weatherData[0].current.wind_mph, slider: false }} windDirection={weatherData[0].current.wind_dir} /> */}
                             <BasicCard title='Wind Status' value={weatherData[0].current.wind_mph} unit={'mph'} message={weatherData[0].current.wind_dir} />
                         </div>
                         <div className=''>
-                            {/* <HighlightCardOne data={{title: 'Sunrise and Sunset', value: weatherData[1].forecastday[0].day.uv, slider: true }}/> */}
                             <SunCard title='Sunrise & Sunset' sunrise={weatherData[1].forecastday[0].astro.sunrise} sunset={weatherData[1].forecastday[0].astro.sunset} />
                         </div>
                         <div className=''>
-                            {/* <HighlightCardOne data={{title: 'UV Index', value: weatherData[1].forecastday[0].day.uv, slider: true }}/> */}
                             <BasicCard title='Humidity' value={weatherData[0].current.humidity} unit={'%'} message={weatherData[0].current.humidity < 50 ? 'Awesome 👍' : 'Could be worse 👎'} />
                         </div>
                         <div className=''>
-                            {/* <HighlightCardOne data={{title: 'UV Index', value: weatherData[1].forecastday[0].day.uv, slider: true }}/> */}
                             <BasicCard title='Visibility' value={weatherData[0].current.vis_miles} unit={'mi'} message={weatherData[0].current.vis_miles < 3 ? 'Helen Keller 👓' : 'Good to go 👀'} />
                         </div>
                         <div className=''>
-                            {/* <HighlightCardOne data={{title: 'UV Index', value: weatherData[1].forecastday[0].day.uv, slider: true }}/> */}
                             <BasicCard title='Feels Like' value={Math.round(weatherData[0].current.feelslike_f)} unit={'°F'} message={Math.round(weatherData[0].current.feelslike_f) < 32 ? 'Brrrrr ⛄️' : 'Sun Bathing? 🌞'}/>
                         </div>
 
