9dff13fc056cdf545dda45975b7ac01515da4671
zachshouts <117788174+zachshouts@users.noreply.github.com>
2023-03-05 15:02:02 -0600

    Changed homepage from package.json

--- src/components/PageLoad.jsx
+++ src/components/PageLoad.jsx
@@ -5,7 +5,8 @@ const PageLoad = () => {
     return (
         <section className='container flex justify-center items-center lg:grid grid-cols-2 lg:justify-items-end lg:items-center h-screen'>
             <div className='text-center'>
-                <Spinner  size='xl' speed='.7s'/>
+                <p className='mb-3 text-xl'>Search For Your City...</p>
+                <Spinner label='Loading...' size='xl' speed='.7s'/>
             </div>
         </section>
     );
